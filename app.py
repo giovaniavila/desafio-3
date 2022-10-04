311dc72ee189ce35576f3953ffaecf74eda55757
gio <112128418+giovaniavila@users.noreply.github.com>
2022-10-04 09:49:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,15 @@ app = Flask(__name__,template_folder='templates', static_folder='static')
 def index():
     return render_template('index.html')
 
+
+@app.route('/quemsomos')
+def somos():
+    return render_template("quemsomos.html")
+
+
+@app.route('/contato')
+def contato():
+    return render_template("contato.html")
+
 if __name__ == '__main__':
     app.run(debug = True)
